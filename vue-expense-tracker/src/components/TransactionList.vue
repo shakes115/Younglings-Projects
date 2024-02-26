5dac0b14955cfa75fa7faf377ac4026621e6a604
shakes115 <shaakier.railoun115@gmail.com>
2024-02-26 12:49:26 +0200

    -created new vue project "vue-expense-tracker"

---
+++ vue-expense-tracker/src/components/TransactionList.vue
@@ -0,0 +1,25 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li class="minus">
+        Cash <span>-R400</span><button
+        class="delete-btn">x</button>
+        </li>  
+        <li class="plus">
+        Paycheck <span>R800</span><button
+        class="delete-btn">x</button>
+        </li> 
+    </ul>
+</template>
+
+<script>
+export default{
+    data() {
+        return{
+            transactions: [
+                { id: 1, text: 'Flower', amount: -19.99} ,
+            ]
+        }
+    }
+}
+</script>
